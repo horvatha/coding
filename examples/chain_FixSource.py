5d1fc3996ae6580bce631c3826452d4e1c22fe36
Arpad Horvath <horvath.arpad.szfvar@gmail.com>
2012-03-18 00:01:55 +0100

    new examples:   chain_FixSource.py chain_Source.py

---
+++ examples/chain_FixSource.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""Example using the package.
+"""
+
+from __future__ import division
+from __future__ import print_function
+
+__author__ = 'Arpad Horvath'
+
+from coding import (Chain, FixSource, Source, Code, Hamming, Channel)
+
+source = FixSource("ALABAMA")
+
+chain = Chain(
+    source,
+    Code("00 01 10 11", symbols=source.symbols),
+    Hamming(4),
+    Channel("1"),
+    verbosity = 1,
+    )
+result = chain.run()
